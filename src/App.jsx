b6b4865825f3bc58e484db208aea311ae89773cd
tawandakasunzuma <tskasunzuma@gmail.com>
2025-07-02 17:57:44 +0200

    scaffold React app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}

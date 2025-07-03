57698878644759b6e0287984f192816c8d3b61a9
tawandakasunzuma <tskasunzuma@gmail.com>
2025-07-03 19:41:08 +0200

    add filter section and card section container

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,13 @@
+import "../styles/Main.css"
+import FilterSection from "./FilterSection"
+
 export default function Main () {
     return (
-        <>
-            Main
-        </>
+        <main>
+            <FilterSection />
+            <section className="card-section">
+                /* Card Section */
+            </section>
+        </main>
     )
 }
